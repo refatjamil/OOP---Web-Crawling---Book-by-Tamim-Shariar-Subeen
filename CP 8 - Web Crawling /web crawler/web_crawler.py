e9e12b2af48f4ba006c611ff0ee706fde8c9c9dd
rifat-jamil <rifatjamil54@gmail.com>
2023-01-29 23:20:12 +0600

    web crawler dir

--- CP 8 - Web Crawling /web crawler/web_crawler.py
+++ CP 8 - Web Crawling /web crawler/web_crawler.py
@@ -7,7 +7,7 @@ import csv
 def get_category_list(content):
     """get_category_list takes content of home page
     and returns a list of categories and their urls"""
-    pass
+    return category_pat.findall(content)
 
 def get_book_list(content):
     """get_book_list takes content of a book list page
@@ -23,7 +23,17 @@ def get_product_details(content):
 
 def get_page_content(url):
     """get_page_content takes a url and returns the content os the pass"""
-    pass
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        logging.error(e) 
+
+    if response.ok:
+        return response.text     
+
+    logging.error("Can not get content from url:" + url)
+
+    return None
 
 def get_next_page(content):
     """Checks the content of a book list page and returns link
@@ -56,13 +66,24 @@ def crawl_website():
     host_name = "books.toscrape.com"
 
     content = get_page_content(url)
+    if content is None:
+        logging.critical("Failed to get content from " + url)
+        sys.exit(1)
+
     category_list = get_category_list(content)
 
     for category in category_list:
-        category_name, category_url = category
+        category_url, category_name = category
+        print(category_url)
+        sys.exit(1)
         crawl_category(category_name, category_url)
 
+
+
 if __name__ == "__main__":
+    # Compile different reguler expresion patterns
+    category_pat = re.compile(r'<li>\s*<a href="(catalogue/category/books/.*?)">\s*(\w+[\s\w]+\w)\s*?<', re.M |re.DOTALL)
+
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S: %p', filename='bookstore_crawler.log', level=logging.DEBUG)
 
     with open("book_list.csv", "w") as csvf:
